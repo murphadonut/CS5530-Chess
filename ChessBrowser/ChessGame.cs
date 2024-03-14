2596c2f5d3aa6218cd705184f32c6c473303191c
murphadonut <murphadonut@gmail.com>
2024-03-13 22:22:46 -0600

    Initial commit

---
+++ ChessBrowser/ChessGame.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBrowser
+{
+    public class ChessGame
+    {
+        public string eventName { private set; get; }
+        public string eventSite { private set; get; }
+        public string eventDate { private set; get; }
+        public string round { private set; get; }
+        public string whiteName { private set; get; }
+        public string blackName { private set; get; }
+        public int whiteELO { private set; get; }
+        public int blackELO { private set; get; }
+        public char result { private set; get; }
+        public string moves { private set; get; }
+
+        public ChessGame(string[] lines)
+        {
+            // Remove any extra blank lines
+            lines = lines.Where(line => !string.IsNullOrEmpty(line)).ToArray();
+            // For checking whether all the other data has been constructed.
+            bool ontoMoves = false;
+            foreach (string line in lines)
+            {
+                // I assumed every moves first line starts with 1
+                if (ontoMoves || line.StartsWith("1"))
+                {
+                    moves = moves + line;
+                    ontoMoves = true;
+                    continue;
+                }
+                // Get last index of tag
+                int last = line.IndexOf(' ');
+                string value = line.Substring(last + 2, line.Length - last - 4);
+                string tag = line.Substring(1, last - 1);
+                switch (tag)
+                {
+                    case "Event":
+                        eventName = value;
+                        break;
+                    case "Site":
+                        eventSite = value;
+                        break;
+                    case "EventDate":
+                        eventDate = value;
+                        if (value.Contains('?'))
+                        {
+                            eventDate = "0000-00-00";
+                        }                     
+                        break;
+                    case "Round":
+                        round = value;
+                        break;
+                    case "White":
+                        whiteName = value;
+                        break;
+                    case "Black":
+                        blackName = value;
+                        break;
+                    case "WhiteElo":
+                        whiteELO = int.Parse(value);
+                        break;
+                    case "BlackElo":
+                        blackELO = int.Parse(value);
+                        break;
+                    case "Result":
+                        switch (value)
+                        {
+                            case "1-0":
+                                result = 'W';
+                                break;
+                            case "0-1":
+                                result = 'B';
+                                break;
+                            case "1/2-1/2":
+                                result = 'D';
+                                break;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        // Only used for debugging
+        override
+        public string ToString()
+        {
+            return "Event: " + eventName + "\nSite: " + eventSite + "\nDate: " + eventDate + "\nRound: " + round + "\nWhite: " + whiteName + "\nBlack: " + blackName + "\nWhiteElo: " + whiteELO + "\nBlackElo " + blackELO + "\nResult: " + result + "\nMoves: " + moves;
+        }
+    }
+}
---
+++ ChessBrowser/PGNReader.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBrowser
+{
+    public static class PGNReader
+    {
+        /// <summary>
+        /// Takes a text file location and attempts to parse it into a ChessGame object
+        /// </summary>
+        /// <param name="fileLocation">Location of the file to open</param>
+        /// <returns>An array of all the chess games in the provided text file</returns>
+        public static ChessGame[] parsePGN(string fileLocation)
+        {
+            List<ChessGame> games = new List<ChessGame>();
+            try
+            {
+                // Get all the lines from the text file and store as array
+                string[] allLines = File.ReadAllLines(fileLocation);
+                // Used to separate games by every other empty line.
+                bool onSecondLine = false;
+                // Store the lines for each separate game one at a time
+                // Is a list because it grows
+                List<string> tempLines = new List<string>();
+                // Go through the whole text document and build string arrays containing all the info
+                // needed for a chess game then construct said chess game 
+                for (int i = 0; i < allLines.Length; i++)
+                {
+                    // Check if its at the end of the file
+                    if (i == allLines.Length - 1)
+                    {
+                        games.Add(new ChessGame(tempLines.ToArray()));
+                        continue;
+                    }
+                    // Check if there is an empty line
+                    if (string.IsNullOrEmpty(allLines[i]))
+                    {
+                        // If the moves tag data has already been added to tempLines, all the info is 
+                        // collected and chess game can be contructed now.
+                        if (onSecondLine)
+                        {
+                            games.Add(new ChessGame(tempLines.ToArray()));
+                            // Reset array to start buildling next game
+                            tempLines.Clear();
+                        }
+                        onSecondLine = !onSecondLine;
+                        continue;
+                    }
+                    tempLines.Add(allLines[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading from file: " + fileLocation + "\n" + ex);
+            }
+            return games.ToArray();
+        }
+    }
+}
